dcd15431edf7fbd9c2eb78a4b6bb9bd7018aa893
Alisher Uralov <aliwerdev@gmail.com>
2023-11-11 16:52:54 +0500

    Updates

---
+++ precache-manifest.6ec6127a55aaf17c382bd84bab3aa7d9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ec05c197bb39fa1092a",
+    "url": "/puzzle15/static/css/main.a2d27da6.chunk.css"
+  },
+  {
+    "revision": "5ec05c197bb39fa1092a",
+    "url": "/puzzle15/static/js/main.4781d47b.chunk.js"
+  },
+  {
+    "revision": "d896ef780c959dae4e7c",
+    "url": "/puzzle15/static/js/runtime~main.9b891d03.js"
+  },
+  {
+    "revision": "98227e2181ff5e774b23",
+    "url": "/puzzle15/static/js/2.41ec6ea8.chunk.js"
+  },
+  {
+    "revision": "9203b416b5aa697c72080ee28a119490",
+    "url": "/puzzle15/static/media/button.9203b416.svg"
+  },
+  {
+    "revision": "d63d02fd9679b91149b3260bd2bdd67a",
+    "url": "/puzzle15/static/media/numberBackground.d63d02fd.svg"
+  },
+  {
+    "revision": "885ded4484de9d5e9d34abf2d65b34aa",
+    "url": "/puzzle15/static/media/close.885ded44.svg"
+  },
+  {
+    "revision": "0df525694831df21de4ab857a8aecd53",
+    "url": "/puzzle15/static/media/board.0df52569.svg"
+  },
+  {
+    "revision": "8378e863aa0226e94ccc2eeba10353f3",
+    "url": "/puzzle15/static/media/gamebackground.8378e863.jpg"
+  },
+  {
+    "revision": "046be122b68821bb61fbc8e5327bdfae",
+    "url": "/puzzle15/index.html"
+  }
+];
